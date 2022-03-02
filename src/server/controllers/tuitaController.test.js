a5833b05daf3ef62c15b68cc94ba1a7ea848ca8f
Adam El Amrani <adelamco1091@outlook.com>
2022-03-03 00:43:44 +0100

    Tried to test updateTuit function. Pending to correct

--- src/server/controllers/tuitaController.test.js
+++ src/server/controllers/tuitaController.test.js
@@ -1,7 +1,40 @@
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const { default: mongoose } = require("mongoose");
 const Tuit = require("../../db/models/Tuit");
-const getAllTuits = require("./tuitahController");
+const { getAllTuits, updateTuit } = require("./tuitahController");
+const connectDataBase = require("../../db/index");
 
 jest.mock("../../db/models/Tuit");
+
+let tuits;
+let dataBase;
+
+beforeAll(async () => {
+  dataBase = await MongoMemoryServer.create();
+  const mongoUrl = dataBase.getUri();
+  await connectDataBase(mongoUrl);
+});
+
+beforeEach(async () => {
+  jest.resetAllMocks();
+  tuits = [
+    {
+      date: "2020",
+      text: "tuitah rules",
+      likes: 0,
+      id: 2,
+    },
+  ];
+});
+
+afterEach(async () => {
+  await Tuit.deleteMany({});
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await dataBase.stop();
+});
 
 describe("Given an getAllTuits controller", () => {
   beforeEach(() => {
@@ -13,13 +46,6 @@ describe("Given an getAllTuits controller", () => {
         json: jest.fn(),
         status: jest.fn().mockReturnThis(),
       };
-      const tuits = [
-        {
-          date: "2020",
-          text: "tuitah rules",
-          likes: 0,
-        },
-      ];
 
       Tuit.find = jest.fn().mockResolvedValue(tuits);
 
@@ -27,6 +53,24 @@ describe("Given an getAllTuits controller", () => {
 
       expect(Tuit.find).toHaveBeenCalled();
       expect(res.json).toHaveBeenCalledWith({ tuits });
+    });
+  });
+});
+
+describe("Given an updateTuit function", () => {
+  describe("When it receives a request with an update in the body", () => {
+    test("Then it should call the method json with with the update", async () => {
+      const id = 2;
+      const update = { text: "hola" };
+      const req = { params: id, body: update };
+      console.log(tuits);
+
+      const res = { status: jest.fn().mockReturnThis, json: jest.fn() };
+      Tuit.findByIdAndUpdate = jest.fn().mockResolvedValue(id, req.body);
+
+      await updateTuit(req, res);
+
+      expect(res.json).toHaveBeenCalled();
     });
   });
 });
