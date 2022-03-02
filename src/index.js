5adc86049dc3f6b93f283acfbdd3c1f112d56de5
Silvia Martinez <sil@silvia.com>
2022-03-02 18:42:37 +0100

    Import app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const debug = require("debug")("Tuitah:root");
 const chalk = require("chalk");
 const upServer = require("./server/upServer");
+const connectDataBase = require("./db");
+const app = require("./server");
 
 const dbstring = process.env.MONGO_DB_STRING;
 const port = process.env.PORT || 4000;
