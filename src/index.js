15ea07f06c2b7cd57c5059a48dc857303831cef7
Silvia Martinez <sil@silvia.com>
2022-03-02 18:34:47 +0100

    Created src/index

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const debug = require("debug")("Tuitah:root");
+const chalk = require("chalk");
+const upServer = require("./server/upServer");
+
+const dbstring = process.env.MONGO_DB_STRING;
+const port = process.env.PORT || 4000;
+
+(async () => {
+  try {
+    await connectDataBase(dbstring);
+    await upServer(port, app);
+  } catch (error) {
+    debug(chalk.red(`Error:`, error.message));
+  }
+})();
