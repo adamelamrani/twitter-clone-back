fee7bd605911c46b7c33e12ed675e8a0594a3baf
Adam El Amrani <adelamco1091@outlook.com>
2022-03-02 23:26:43 +0100

    Created update tuit controller

--- src/server/routers/tuitahRouter.js
+++ src/server/routers/tuitahRouter.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const { getAllTuits, createTuit } = require("../controllers/tuitahController");
+const {
+  getAllTuits,
+  createTuit,
+  updateTuit,
+} = require("../controllers/tuitahController");
 
 const router = express.Router();
 
 router.get("/list", getAllTuits);
-
+router.patch("/id", updateTuit);
 router.post("/new", createTuit);
 
 module.exports = router;
